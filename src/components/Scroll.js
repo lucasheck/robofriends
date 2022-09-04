c8b80e8a69e0f579c6fc85ff6b3d8fa3a630cce3
lucasheck <lucasheck23@gmail.com>
2022-09-04 18:18:36 -0400

    Shrinking the size of tha page.

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ import React from "react";
 const Scroll = (props) => {
 	/*prettier-ignore*/
 	return (
-        <div style={{ overflowY: "scroll", height: "800px", border: "solid black 1px" }}>
+        <div style={{ overflowY: "scroll", height: "700px", border: "solid black 1px" }}>
             {props.children}
         </div>
     );
